1d5f996f759fa25c9539c4a7601ed6c33dfe1b72
PikalaxALT <PikalaxALT@users.noreply.github.com>
2024-12-13 17:16:59 -0500

    day 13

--- src/aoc2024_13.cpp
+++ src/aoc2024_13.cpp
@@ -1,12 +1,55 @@
+#include <regex>
 #include "aoc2024.hpp"
 
 class Day13 : public aoc2024::Impl {
+    struct Machine {
+        using coord2ull = std::pair<unsigned long long, unsigned long long>;
+        coord2ull button_a;
+        coord2ull button_b;
+        coord2ull prize;
+    };
+    std::vector<Machine> machines;
 public:
-    Day13 (const std::string &input) : aoc2024::Impl(input) {}
+    Day13 (const std::string &input) : aoc2024::Impl(input) {
+        std::string input_c = input;
+        std::regex pattern {R"(Button A: X\+(\d+), Y\+(\d+)\nButton B: X\+(\d+), Y\+(\d+)\nPrize: X=(\d+), Y=(\d+))"};
+        for (std::smatch match; std::regex_search(input_c, match, pattern);) {
+            machines.emplace_back(
+                Machine::coord2ull{std::stoull(match[1]), std::stoull(match[2])},
+                Machine::coord2ull{std::stoull(match[3]), std::stoull(match[4])},
+                Machine::coord2ull{std::stoull(match[5]), std::stoull(match[6])}
+            );
+            input_c = match.suffix();
+        }
+    }
 
-    void part1 () final {}
+    unsigned long long getPrize (const Machine &machine, unsigned long long offset = 0) {
+        auto prize_x = machine.prize.first + offset;
+        auto prize_y = machine.prize.second + offset;
+        long long denom = machine.button_a.first * machine.button_b.second - machine.button_a.second * machine.button_b.first;
+        long long numer_a = prize_x * machine.button_b.second - prize_y * machine.button_b.first;
+        long long numer_b = machine.button_a.first * prize_y - machine.button_a.second * prize_x;
+        if (numer_a % denom == 0 && numer_b % denom == 0) {
+            return (numer_a / denom) * 3 + (numer_b / denom);
+        }
+        return 0;
+    }
 
-    void part2 () final {}
+    void part1 () final {
+        unsigned long long result = 0ull;
+        for (const auto &machine : machines) {
+            result += getPrize(machine);
+        }
+        std::cout << result << std::endl;
+    }
+
+    void part2 () final {
+        unsigned long long result = 0ull;
+        for (const auto &machine : machines) {
+            result += getPrize(machine, 10000000000000);
+        }
+        std::cout << result << std::endl;
+    }
 };
 
 int main () {

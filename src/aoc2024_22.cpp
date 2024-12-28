7ad5ad22374ce3813ea344bc56b2dfe48af9c455
PikalaxALT <PikalaxALT@users.noreply.github.com>
2024-12-28 15:32:18 -0500

    finish day 22

--- src/aoc2024_22.cpp
+++ src/aoc2024_22.cpp
@@ -1,5 +1,7 @@
 #include <ranges>
 #include <charconv>
+#include <map>
+#include <set>
 #include "aoc2024.hpp"
 
 class Day22 : public aoc2024::Impl {
@@ -17,20 +19,56 @@ public:
             | std::ranges::to<std::vector>();
     }
 
+    constexpr unsigned long long evolve(unsigned long long x) const {
+        x = (x ^ (x << 6)) & 0xFFFFFF;
+        x = (x ^ (x >> 5)) & 0xFFFFFF;
+        x = (x ^ (x << 11)) & 0xFFFFFF;
+        return x;
+    }
+
     void part1 () final {
         unsigned long long result = 0;
         for (auto x : initialSecretNumbers) {
             for (int i = 0; i < 2000; ++i) {
-                x = (x ^ (x << 6)) & 0xFFFFFF;
-                x = (x ^ (x >> 5)) & 0xFFFFFF;
-                x = (x ^ (x << 11)) & 0xFFFFFF;
+                x = evolve(x);
             }
             result += x;
         }
         std::cout << result << std::endl;
     }
 
-    void part2 () final {}
+    void part2 () final {
+        std::map<std::array<int, 4>, unsigned long long> patterns;
+        for (int i = 0; auto x : initialSecretNumbers) {
+            std::set<std::array<int, 4>> seen;
+            std::array<int, 4> sequence;
+            std::array<int, 5> mod10s;
+            mod10s[0] = x % 10;
+            int j;
+            for (j = 0; j < 4; ++j) {
+                x = evolve(x);
+                mod10s[j + 1] = x % 10;
+                sequence[j] = mod10s[j + 1] - mod10s[j];
+            }
+            for (; j < 2000; ++j) {
+                if (seen.insert(sequence).second) {
+                    patterns[sequence] += mod10s[4];
+                }
+                std::move(sequence.begin() + 1, sequence.end(), sequence.begin());
+                std::move(mod10s.begin() + 1, mod10s.end(), mod10s.begin());
+                x = evolve(x);
+                mod10s[4] = x % 10;
+                sequence[3] = mod10s[4] - mod10s[3];
+            }
+            ++i;
+        }
+        auto kvpair = std::ranges::max(std::views::keys(patterns), [&](const auto &a, const auto &b) {
+            return patterns.at(a) < patterns.at(b);
+        });
+        std::cerr << "{";
+        aoc2024::print(std::cerr, kvpair, ",") << "}\n";
+        std::cout << patterns.at(kvpair) << std::endl;
+    }
 };
 
 int main () {
